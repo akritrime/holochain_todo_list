25756bc43cd2f718569a28b5546148a229ca7cb1
akritrime <akritrime@gmail.com>
2018-11-12 17:52:59 +0530

    master

--- test/index.js
+++ test/index.js
@@ -22,25 +22,20 @@ app.start()
 
 
 test('Can create a list', (t) => {
-  const create_result = app.call("lists", "main", "create_list", JSON.stringify({name: "test list"}))
-  // t.equal(JSON.parse(create_result).success, true)
-  console.log(create_result)
+  const create_result = app.call("lists", "main", "create_list", JSON.stringify({list: {name: "test list"}}))
+  t.equal(JSON.parse(create_result).success, true)
   t.end()
 })
 
 test('Can add some items', (t) => {
   const create_result = app.call("lists", "main", "create_list", JSON.stringify({list: {name: "test list"}}))
   const list_addr = JSON.parse(create_result).address
-  console.log(list_addr)
 
   const result1 = app.call("lists", "main", "add_item", JSON.stringify({list_item: {text: "Learn Rust", completed: true}, list_addr: list_addr}))
   const result2 = app.call("lists", "main", "add_item", JSON.stringify({list_item: {text: "Master Holochain", completed: false}, list_addr: list_addr}))
 
-  console.log(result1)
-  console.log(result2)
-
-  // t.equal(JSON.parse(result1).success, true)
-  // t.equal(JSON.parse(result2).success, true)
+  t.equal(JSON.parse(result1).success, true)
+  t.equal(JSON.parse(result2).success, true)
 
   t.end()
 })
@@ -54,7 +49,6 @@ test('Can get a list with items', (t) => {
 
   const get_result = app.call("lists", "main", "get_list", JSON.stringify({list_addr: list_addr}))
 
-  // t.equal(JSON.parse(get_result).result.length, 2)
-  console.log(get_result)
+  t.equal(JSON.parse(get_result).items.length, 2)
   t.end()
 })
--- zomes/lists/code/src/lib.rs
+++ zomes/lists/code/src/lib.rs
@@ -1,88 +1,66 @@
 #[macro_use]
 extern crate hdk;
-// #[macro_use]
+#[macro_use]
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 #[macro_use]
 extern crate serde_json;
 
+// see https://holochain.github.io/rust-api/0.0.1/hdk/ for info on using the hdk library
 use hdk::holochain_dna::zome::entry_types::Sharing;
 use hdk::holochain_core_types::hash::HashString;
-use hdk::holochain_core_types::entry::Entry;
-use hdk::holochain_core_types::entry_type::EntryType;
-use hdk::holochain_core_types::cas::content::Address;
-// use serde::Serialize;
-use hdk::holochain_core_types::json::JsonString;
 
-
-// see https://holochain.github.io/rust-api/0.0.1/hdk/ for info on using the hdk library
-
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize)]
 struct List {
-    name: String
+	name: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize)]
 struct ListItem {
-    text: String,
-    completed: bool
+	text: String,
+	completed: bool
 }
 
-fn handle_create_list(list: List) -> JsonString {
-    let entry = Entry::new(EntryType::App("list".into()), json!(list));
-    match hdk::commit_entry(&entry) {
-        Ok(address) => JsonString::from(json!({
-            "success": true,
-            "address": address
-        })),
-
-        Err(hdk_err) => JsonString::from(json!({
-            "success": false,
-            "error": hdk_err
-        }))
-    }
+fn handle_create_list(list: List) -> serde_json::Value {
+	match hdk::commit_entry("list", json!(list)) {
+		Ok(address) => json!({"success": true, "address": address}),
+		Err(hdk_err) => json!({"success": false, "error": hdk_err})
+	}
 }
 
-fn handle_add_item(list_item: ListItem, list_addr: HashString) -> JsonString {
-    let entry = Entry::new(EntryType::App("listItem".into()), json!(list_item));
-    match hdk::commit_entry(&entry)
-        .and_then(|item_addr| {
-            hdk::link_entries(&list_addr, &item_addr, "items")
-        })
-    {
-        Ok(_) => {
-            JsonString::from(json!({"success": true}))
-        },
-        Err(hdk_err) => JsonString::from(json!({"success": false, "error": hdk_err}))
-    }
+fn handle_add_item(list_item: ListItem, list_addr: HashString) -> serde_json::Value {
+	match hdk::commit_entry("listItem", json!(list_item))
+		.and_then(|item_addr| {
+			hdk::link_entries(&list_addr, &item_addr, "items")
+		})
+	{
+		Ok(_) => {
+			json!({"success": true})
+		},
+		Err(hdk_err) => json!({"success": false, "error": hdk_err})
+	}
 }
 
-fn handle_get_list(list_addr: Address) -> JsonString {
-    match hdk::get_entry(list_addr.clone()) {
-        Ok(Some(_list)) => {
-            match hdk::get_links(&list_addr, "items") {
-                Ok(result) => {
-                    JsonString::from(json!({
-                        "success": true,
-                        "result": result
-                    }))
-                }
+fn handle_get_list(list_addr: HashString) -> serde_json::Value {
+	match hdk::get_entry::<List>(list_addr.clone()) { // try and get the list
+		Ok(Some(list)) => {
+			match hdk::get_links(&list_addr, "items") { // if successful, try to load the linked items
+				Ok(result) => {
+					let items: Vec<ListItem> = result.links.iter()
+						.map(|item_addr| hdk::get_entry(item_addr.to_owned()) )
+						.filter_map(|elem: Result<Option<ListItem>, _>| elem.unwrap())
+						.collect(); // collect all the Ok items in to a list
 
-                Err(hdk_err) => JsonString::from(json!({
-                    "success": false,
-                    "error": hdk_err
-                }))
-            }
-        },
-
-        Ok(None) => JsonString::from(json!({"success": false, "error": "no list found"})),
-        Err(hdk_err) => JsonString::from(json!({
-            "success": false,
-            "error": hdk_err
-        }))
-    }
-}
+					json!({"name": list.name, "items": items})
+				},
+				Err(hdk_err) => hdk_err.to_json()
+			}
+		},
+		Ok(None) => json!({"error": "no list found at address"}),
+		Err(hdk_err) => hdk_err.to_json()
+	}
+}	
 
 define_zome! {
     entries: [
@@ -102,7 +80,7 @@ define_zome! {
             sharing: Sharing::Public,
             native_type: ListItem,
             validation_package: || hdk::ValidationPackageDefinition::Entry,
-            validation: |list_item: ListItem, _ctx:  hdk::ValidationData| {
+            validation: |list_item: ListItem, _ctx: hdk::ValidationData| {
                 Ok(())
             }
         )
@@ -111,20 +89,22 @@ define_zome! {
     genesis: || { Ok(()) }
 
     functions: {
+        // "main" is the name of the group
+        // "Public" is the access setting of the group
         main (Public) {
             create_list: {
                 inputs: |list: List|,
-                outputs: |result: JsonString|,
+                outputs: |result: serde_json::Value|,
                 handler: handle_create_list
             }
             add_item: {
                 inputs: |list_item: ListItem, list_addr: HashString|,
-                outputs: |result: JsonString|,
+                outputs: |result: serde_json::Value|,
                 handler: handle_add_item
             }
             get_list: {
                 inputs: |list_addr: HashString|,
-                outputs: |result: JsonString|,
+                outputs: |result: serde_json::Value|,
                 handler: handle_get_list
             }
         }
